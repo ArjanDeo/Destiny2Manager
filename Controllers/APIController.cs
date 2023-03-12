37d340ba3d040298d58639aa8a123972e173fb70
Sam Deo <samdeo@bluecomet-it.com>
2023-03-11 21:16:35 -0800

    Fixed Auth

--- Controllers/APIController.cs
+++ Controllers/APIController.cs
@@ -3,68 +3,82 @@ using Newtonsoft.Json;
 using Destiny2ManagerMVC.Models;
 using System.Collections.Specialized;
 using System.Web;
+using System.Text;
+using Pathoschild.Http.Client;
 
 namespace Destiny2ManagerMVC.Controllers
 {
     public class APIController : Controller
     {
         private readonly string apikey = "cbd862bffe084ca896f9ee6371a6bbe3";
-		private readonly string clientSecret = "";
-		public async Task<IActionResult> DestinyInfo()
-		{
-			#region API Links
-			string CharInfo = "https://www.bungie.net/Platform/Destiny2/5/Profile/4611686018495570819/Character/2305843009776964460/?components=200";
-			string GjallarhornD1 = "https://www.bungie.net/platform/Destiny/Manifest/InventoryItem/1274330687/?components=300";
-			string SanitizedPlatformUN = "https://www.bungie.net/Platform/User/GetSanitizedPlatformDisplayNames/4611686018495570819/";
-			#endregion
-			#region API Request / Deserialization
-			HttpClient client = new HttpClient();
-			client.DefaultRequestHeaders.Add("x-api-key", apikey);
-			HttpResponseMessage responseRoute = await client.GetAsync(CharInfo);
-			responseRoute.EnsureSuccessStatusCode();
-			var responseRouteBody = await responseRoute.Content.ReadAsStringAsync();
-			var jsonDeserialize = JsonConvert.DeserializeObject<CharacterDataAPIModel>(responseRouteBody);
-			var playTime = "";
-			int lightlevel = 0;
-			string lastplayedDT = "";
-			var emblemPath = "";
-			var steamUser = "";
-			#endregion
-			#region API Info Registration
-			if (jsonDeserialize != null)
-			{
-				emblemPath = jsonDeserialize.Response.character.data.emblemPath;
-				playTime = jsonDeserialize.Response.character.data.minutesPlayedTotal;
-				lightlevel = jsonDeserialize.Response.character.data.light;
-				var lastplayed = jsonDeserialize.Response.character.data.dateLastPlayed;
-				lastplayedDT = Convert.ToDateTime(lastplayed).ToString("dd/MM/yyyy");
-				steamUser = jsonDeserialize.Response.SteamId;
-			}
+        private readonly string clientSecret = "";
+        public async Task<IActionResult> DestinyInfo()
+        {
+            #region API Links
+            string CharInfo = "https://www.bungie.net/Platform/Destiny2/5/Profile/4611686018495570819/Character/2305843009776964460/?components=200";
+            string GjallarhornD1 = "https://www.bungie.net/platform/Destiny/Manifest/InventoryItem/1274330687/?components=300";
+            string SanitizedPlatformUN = "https://www.bungie.net/Platform/User/GetSanitizedPlatformDisplayNames/4611686018495570819/";
+            #endregion
+            #region API Request / Deserialization
+            HttpClient client = new HttpClient();
+            client.DefaultRequestHeaders.Add("x-api-key", apikey);
+            HttpResponseMessage responseRoute = await client.GetAsync(CharInfo);
+            responseRoute.EnsureSuccessStatusCode();
+            var responseRouteBody = await responseRoute.Content.ReadAsStringAsync();
+            var jsonDeserialize = JsonConvert.DeserializeObject<CharacterDataAPIModel>(responseRouteBody);
+            var playTime = "";
+            int lightlevel = 0;
+            string lastplayedDT = "";
+            var emblemPath = "";
+            var steamUser = "";
+            #endregion
+            #region API Info Registration
+            if (jsonDeserialize != null)
+            {
+                emblemPath = jsonDeserialize.Response.character.data.emblemPath;
+                playTime = jsonDeserialize.Response.character.data.minutesPlayedTotal;
+                lightlevel = jsonDeserialize.Response.character.data.light;
+                var lastplayed = jsonDeserialize.Response.character.data.dateLastPlayed;
+                lastplayedDT = Convert.ToDateTime(lastplayed).ToString("dd/MM/yyyy");
+                steamUser = jsonDeserialize.Response.SteamId;
+            }
             #endregion
             var pt = Convert.ToInt32(playTime) / 60;
-			ViewData["totalplayed"] = pt.ToString();
-			ViewData["lightlevel"] = lightlevel;
-			ViewData["lastplayed"] = lastplayedDT;
-			ViewData["emblempath"] = $"https://www.bungie.net/{emblemPath}";
-			ViewData["steamuser"] = steamUser;
-			return View();
-		}
-	public async Task<IActionResult> Callback(string code)
+            ViewData["totalplayed"] = pt.ToString();
+            ViewData["lightlevel"] = lightlevel;
+            ViewData["lastplayed"] = lastplayedDT;
+            ViewData["emblempath"] = $"https://www.bungie.net/{emblemPath}";
+            ViewData["steamuser"] = steamUser;
+            return View();
+        }
+        public async Task<IActionResult> Callback(string code)
         {
-			var client = new HttpClient();
-			var request = new HttpRequestMessage(HttpMethod.Post, "https://www.bungie.net/Platform/App/OAuth/Token");
-			request.Headers.Add("Authorization", "Basic NDMyMDk6aFFCZWVMWmc4SmhWdWxNU1llNFJJRHJIZFRkLmt2ZkM3NzIuc0QuTGpZdw==");
-			var collection = new List<KeyValuePair<string, string>>();
-			collection.Add(new("grant_type", "authorization_code"));
-			collection.Add(new("code", code));
-			var content = new FormUrlEncodedContent(collection);
-			request.Content = content;
-			var response = await client.SendAsync(request);
-			response.EnsureSuccessStatusCode();
-			return Ok(code);
+            FluentClient client = new FluentClient();
+            IResponse ResponseData = await client.PostAsync("https://www.bungie.net/platform/app/oauth/token/")
+                .WithOptions(ignoreHttpErrors: true)
+                .WithBasicAuthentication("43209", "hQBeeLZg8JhVulMSYe4RIDrHdTd.kvfC772.sD.LjYw")
+                .WithBody(x => x.FormUrlEncoded(new BungieOAuthTokenRequestModel
+                {
+                    grant_type = "authorization_code",
+                    code = code
+                }));
+
+            if (ResponseData.IsSuccessStatusCode)
+            {
+
+            }
+            else
+            {
+
+            }
+
+            BungieOAuthTokenResponseModel ResponseDataModel = await ResponseData.As<BungieOAuthTokenResponseModel>();
+
+            return Ok(ResponseDataModel);
 
         }
-		public async Task<IActionResult> Index()
+
+        public async Task<IActionResult> Index()
         {
             #region API Links
             string CharInfo = "https://www.bungie.net/Platform/Destiny2/5/Profile/4611686018495570819/Character/2305843009776964460/?components=200";
@@ -103,6 +117,6 @@ namespace Destiny2ManagerMVC.Controllers
             return View();
         }
 
-   
+
     }
 }
---
+++ Models/BungieOAuthTokenRequestModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Destiny2ManagerMVC.Models
+{
+    public class BungieOAuthTokenRequestModel
+    {
+        public string grant_type { get; set; }
+        public string code { get; set; }
+    }
+}
