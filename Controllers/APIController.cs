3e246bb52b67dec6c449099b08a113371712337b
ArjanDeo <arjandeo@bluecomet-it.com>
2023-04-08 22:29:12 +0100

    Checking if user is authorized.

--- Controllers/APIController.cs
+++ Controllers/APIController.cs
@@ -11,9 +11,9 @@ namespace Destiny2ManagerMVC.Controllers
 {
     public class APIController : Controller
     {
+        bool authorized = false;
 
         private readonly string apikey = "cbd862bffe084ca896f9ee6371a6bbe3";
-        private readonly string clientSecret = "";
         private readonly FluentClient client;
         public APIController()
         {
@@ -26,51 +26,50 @@ namespace Destiny2ManagerMVC.Controllers
             {
                 Response.Redirect("https://www.bungie.net/en/OAuth/Authorize/?client_id=43209&response_type=code");
             }
-             if (BungieConstants.Auth != null)
+            if (BungieConstants.Auth != null)
             {
-                 
-                    IResponse UserDataResponse = await client
-                        .GetAsync("https://www.bungie.net/Platform/User/GetCurrentBungieNetUser/")
-                        .WithOptions(ignoreHttpErrors: true)
-                        .WithHeader("x-api-key", apikey)
-                        .WithBearerAuthentication(BungieConstants.Auth.access_token);
+                authorized = true;
+                ViewData["johnnypork"] = authorized;
 
-                    BungieUserModel ResponseDataModel = await UserDataResponse.As<BungieUserModel>();
+                IResponse UserDataResponse = await client
+                  .GetAsync("https://www.bungie.net/Platform/User/GetCurrentBungieNetUser/")
+                  .WithOptions(ignoreHttpErrors: true)
+                  .WithHeader("x-api-key", apikey)
+                  .WithBearerAuthentication(BungieConstants.Auth.access_token);
 
+                BungieUserModel ResponseDataModel = await UserDataResponse.As<BungieUserModel>();
 
-                    IResponse BungieUserMembershipsResponse = await client
-                        .GetAsync($"https://www.bungie.net/Platform/User/GetMembershipsById/{ResponseDataModel.Response.membershipId}/5/")
-                        .WithOptions(ignoreHttpErrors: true)
-                        .WithHeader("x-api-key", apikey)
-                        .WithBearerAuthentication(BungieConstants.Auth.access_token);
+                IResponse BungieUserMembershipsResponse = await client
+                  .GetAsync($"https://www.bungie.net/Platform/User/GetMembershipsById/{ResponseDataModel.Response.membershipId}/5/")
+                  .WithOptions(ignoreHttpErrors: true)
+                  .WithHeader("x-api-key", apikey)
+                  .WithBearerAuthentication(BungieConstants.Auth.access_token);
 
-                    BungieUserMembershipsModel MembershipData = await BungieUserMembershipsResponse.As<BungieUserMembershipsModel>();
-                    DestinyMembership? Membership = MembershipData.Response.destinyMemberships.FirstOrDefault(x => x.membershipType is 3 or 5);
-                  
+                BungieUserMembershipsModel MembershipData = await BungieUserMembershipsResponse.As<BungieUserMembershipsModel>();
+                DestinyMembership? Membership = MembershipData.Response.destinyMemberships.FirstOrDefault(x => x.membershipType is 3 or 5);
 
                 if (Membership != null)
-                    {
-                        IResponse GetHistoricalStatsForAccount = await client 
-                            .GetAsync($"https://www.bungie.net/Platform/Destiny2/{Membership.membershipType}/Account/{Membership.membershipId}/Stats/")
-                            .WithOptions(ignoreHttpErrors: true)
-                            .WithHeader("x-api-key", apikey);
+                {
+                    IResponse GetHistoricalStatsForAccount = await client
+                      .GetAsync($"https://www.bungie.net/Platform/Destiny2/{Membership.membershipType}/Account/{Membership.membershipId}/Stats/")
+                      .WithOptions(ignoreHttpErrors: true)
+                      .WithHeader("x-api-key", apikey);
 
-                        IResponse CharactersDataResponse = await client
-                            .GetAsync($"https://www.bungie.net/Platform/Destiny2/{Membership.membershipType}/Profile/{Membership.membershipId}/?components=200")
-                            .WithOptions(ignoreHttpErrors: true)
-                            .WithHeader("x-api-key", apikey)
-                            .WithBearerAuthentication(BungieConstants.Auth.access_token);
+                    IResponse CharactersDataResponse = await client
+                      .GetAsync($"https://www.bungie.net/Platform/Destiny2/{Membership.membershipType}/Profile/{Membership.membershipId}/?components=200")
+                      .WithOptions(ignoreHttpErrors: true)
+                      .WithHeader("x-api-key", apikey)
+                      .WithBearerAuthentication(BungieConstants.Auth.access_token);
 
-                        BungieCharacterDataModel CharacterData = await CharactersDataResponse.As<BungieCharacterDataModel>();
-				    	BungieHistoricalStatsModel HistoricalStats = await GetHistoricalStatsForAccount.As<BungieHistoricalStatsModel>();
-                        
+                    BungieCharacterDataModel CharacterData = await CharactersDataResponse.As<BungieCharacterDataModel>();
+                    BungieHistoricalStatsModel HistoricalStats = await GetHistoricalStatsForAccount.As<BungieHistoricalStatsModel>();
+
                     // 0 = Titan
                     // 1 = Hunter
                     // 2 = Warlock
                     KeyValuePair<long, CharacterData> WarlockData = CharacterData.Response.characters.data.FirstOrDefault(x => x.Value.classType == 2);
-                //    KeyValuePair<long, BungieHistoricalStatsModel> HistoricalStatsData = HistoricalStats.Response();
                     KeyValuePair<long, CharacterData> HunterData = CharacterData.Response.characters.data.FirstOrDefault(x => x.Value.classType == 1);
-				    	KeyValuePair<long, CharacterData> TitanData = CharacterData.Response.characters.data.FirstOrDefault(x => x.Value.classType == 0);
+                    KeyValuePair<long, CharacterData> TitanData = CharacterData.Response.characters.data.FirstOrDefault(x => x.Value.classType == 0);
                     BungieEquipItemModel EquipPayload = new BungieEquipItemModel()
                     {
                         itemId = 6917529859030182332,
@@ -79,109 +78,96 @@ namespace Destiny2ManagerMVC.Controllers
 
                     };
                     IResponse GetItemData = await client
-                               .GetAsync($"https://www.bungie.net/Platform/Destiny2/5/Profile/{ResponseDataModel.Response.membershipId}/Character/{WarlockData.Value.characterId}/?components=205")
-                               .WithOptions(ignoreHttpErrors: true)
-                               .WithHeader("x-api-key", apikey);
-                         PHItemDataModel ItemData = await GetItemData.As<PHItemDataModel>();
-
+                      .GetAsync($"https://www.bungie.net/Platform/Destiny2/5/Profile/{ResponseDataModel.Response.membershipId}/Character/{WarlockData.Value.characterId}/?components=205")
+                      .WithOptions(ignoreHttpErrors: true)
+                      .WithHeader("x-api-key", apikey);
+                    BungieItemDataModel ItemData = await GetItemData.As<BungieItemDataModel>();
                     double thoursPlayed = 0.0;
-					double hhoursPlayed = 0.0;
-					double whoursPlayed = 0.0;
+                    double hhoursPlayed = 0.0;
+                    double whoursPlayed = 0.0;
 
-                    if (GetItemData != null) {
-                        IResponse EquipAurvandil = await client
-                                .PostAsync("https://www.bungie.net/Platform/Destiny2/Actions/Items/EquipItem/")
-                                .WithOptions(ignoreHttpErrors: true)
-                                //.WithHeader("Content-Type", "application/json")
-                                .WithHeader("x-api-key", apikey)
-                                .WithBody(EquipPayload)
-                                .WithBearerAuthentication(BungieConstants.Auth.access_token);
-                        //string ChromaRush = $"https://www.bungie.net/{}"
-
-                    }
-                    if (HistoricalStats != null) {
-                        string publiceventscompleted = $"https://www.bungie.net/{HistoricalStats.Response.mergedAllCharacters.results.allPvE.allTime.heroicPublicEventsCompleted}";
-                        ViewData["pec"] = publiceventscompleted;
-                    
-                    }
-					if (TitanData.Value != null)
+                    if (GetItemData != null)
                     {
-                            string titanemblembkg = $"https://www.bungie.net{TitanData.Value.emblemBackgroundPath}";
-                            string titanemblem = $"https://www.bungie.net{TitanData.Value.emblemPath}";
-						    ViewData["titanemblembkg"] = titanemblembkg;
-                            ViewData["titanemblem"] = titanemblem;
-						    int minutesPlayed = int.Parse(TitanData.Value.minutesPlayedTotal);
-						     thoursPlayed = minutesPlayed / 60.0;
-						    ViewData["tpt"] = thoursPlayed.ToString("F2");
-					    	ViewData["tll"] = TitanData.Value.light;
-					}
+                        IResponse EquipAurvandil = await client
+                          .PostAsync("https://www.bungie.net/Platform/Destiny2/Actions/Items/EquipItem/")
+                          .WithOptions(ignoreHttpErrors: true)
+                       //   .WithHeader("Content-Type", "application/json")
+                          .WithHeader("x-api-key", apikey)
+                          .WithBody(EquipPayload)
+                          .WithBearerAuthentication(BungieConstants.Auth.access_token);
+                        //string ChromaRush = $"https://www.bungie.net/{}"
+                        //ViewData["pee"] = ItemData.Response.equipment.data.items.itemInstanceId;
+
+                    }
+                    if (HistoricalStats != null)
+                    {
+                        ViewData["deaths"] = HistoricalStats.Response.mergedAllCharacters.results.allPvE.allTime.deaths.basic.displayValue;
+
+                    }
+                    if (TitanData.Value != null)
+                    {
+                        string titanemblembkg = $"https://www.bungie.net{TitanData.Value.emblemBackgroundPath}";
+                        string titanemblem = $"https://www.bungie.net{TitanData.Value.emblemPath}";
+                        ViewData["titanemblembkg"] = titanemblembkg;
+                        ViewData["titanemblem"] = titanemblem;
+                        int minutesPlayed = int.Parse(TitanData.Value.minutesPlayedTotal);
+                        thoursPlayed = minutesPlayed / 60.0;
+                        ViewData["tpt"] = thoursPlayed.ToString("F2");
+                        ViewData["tll"] = TitanData.Value.light;
+                    }
                     if (HunterData.Value != null)
                     {
-						    string hunteremblembkg = $"https://www.bungie.net{HunterData.Value.emblemBackgroundPath}";
-						    string hunteremblem = $"https://www.bungie.net{HunterData.Value.emblemPath}";
-						    ViewData["hunteremblembkg"] = hunteremblembkg;
-                            ViewData["hunteremblem"] = hunteremblem;
-						    int minutesPlayed = int.Parse(HunterData.Value.minutesPlayedTotal);
-						     hhoursPlayed = minutesPlayed / 60.0;
-						    ViewData["hpt"] = hhoursPlayed.ToString("F2");
-						    ViewData["hll"] = HunterData.Value.light;
+                        string hunteremblembkg = $"https://www.bungie.net{HunterData.Value.emblemBackgroundPath}";
+                        string hunteremblem = $"https://www.bungie.net{HunterData.Value.emblemPath}";
+                        ViewData["hunteremblembkg"] = hunteremblembkg;
+                        ViewData["hunteremblem"] = hunteremblem;
+                        int minutesPlayed = int.Parse(HunterData.Value.minutesPlayedTotal);
+                        hhoursPlayed = minutesPlayed / 60.0;
+                        ViewData["hpt"] = hhoursPlayed.ToString("F2");
+                        ViewData["hll"] = HunterData.Value.light;
 
-					}
-					if (WarlockData.Value != null)
+                    }
+                    if (WarlockData.Value != null)
                     {
-                      
-                            
 
                         string warlockemblembkg = $"https://www.bungie.net{WarlockData.Value.emblemBackgroundPath}";
                         string warlockemblem = $"https://www.bungie.net{WarlockData.Value.emblemPath}";
-						ViewData["wll"] = WarlockData.Value.light;
-						int minutesPlayed = int.Parse(WarlockData.Value.minutesPlayedTotal);
-						 whoursPlayed = minutesPlayed / 60.0;
-						ViewData["wlpt"] = whoursPlayed.ToString("F2");
-						ViewData["lastplayed"] = WarlockData.Value.dateLastPlayed.Date.ToString("dd/MM/yyyy");
-						ViewData["warlockemblembkg"] = warlockemblembkg;
-						ViewData["warlockemblem"] = warlockemblem;
+                        ViewData["wll"] = WarlockData.Value.light;
+                        int minutesPlayed = int.Parse(WarlockData.Value.minutesPlayedTotal);
+                        whoursPlayed = minutesPlayed / 60.0;
+                        ViewData["wlpt"] = whoursPlayed.ToString("F2");
+                        ViewData["lastplayed"] = WarlockData.Value.dateLastPlayed.Date.ToString("dd/MM/yyyy");
+                        ViewData["warlockemblembkg"] = warlockemblembkg;
+                        ViewData["warlockemblem"] = warlockemblem;
 
+                    }
+                    double totalHoursPlayed = whoursPlayed + thoursPlayed + hhoursPlayed;
+                    ViewData["pt"] = totalHoursPlayed.ToString("F2");
 
-					}
-					double totalHoursPlayed = whoursPlayed + thoursPlayed + hhoursPlayed;
-					ViewData["pt"] = totalHoursPlayed.ToString("F2");
-
-
-				}
-			}
+                }
+            }
             return View();
-          
-            //        emblemPath = jsonDeserialize.Response.character.data.emblemPath;
-            //        playTime = jsonDeserialize.Response.character.data.minutesPlayedTotal;
-            //        lightlevel = jsonDeserialize.Response.character.data.light;
-            //        var lastplayed = jsonDeserialize.Response.character.data.dateLastPlayed;
-            //        lastplayedDT = Convert.ToDateTime(lastplayed).ToString("dd/MM/yyyy");
-            //        steamUser = jsonDeserialize.Response.SteamId;
-
         }
         public async Task<IActionResult> Callback(string code)
         {
-            IResponse ResponseData = await client.PostAsync("https://www.bungie.net/platform/app/oauth/token/")
-                .WithOptions(ignoreHttpErrors: true)
-                .WithBasicAuthentication("43209", "hQBeeLZg8JhVulMSYe4RIDrHdTd.kvfC772.sD.LjYw")
-                .WithBody(x => x.FormUrlEncoded(new BungieOAuthTokenRequestModel
-                {
-                    grant_type = "authorization_code",
-                    code = code
-                }));
+            IResponse ResponseData = await client
+              .PostAsync("https://www.bungie.net/platform/app/oauth/token/")
+              .WithOptions(ignoreHttpErrors: true)
+              .WithBasicAuthentication("43209", "hQBeeLZg8JhVulMSYe4RIDrHdTd.kvfC772.sD.LjYw")
+              .WithBody(x => x.FormUrlEncoded(new BungieOAuthTokenRequestModel
+              {
+                  grant_type = "authorization_code",
+                  code = code
+              }));
             BungieOAuthTokenResponseModel ResponseDataModel = await ResponseData.As<BungieOAuthTokenResponseModel>();
             BungieConstants.Auth = ResponseDataModel;
-
+            ViewBag.ResponseData = ResponseDataModel;
             return RedirectToAction("DestinyInfo");
-
         }
-
-        public  IActionResult Index()
+        public IActionResult Index()
         {
             return View();
         }
-
 
     }
 }
--- Models/BungieItemDataModel.cs
+++ Models/BungieItemDataModel.cs
@@ -3,17 +3,10 @@
     // Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(myJsonResponse);
     public class Data
     {
-        public List<Item> items { get; set; }
-        public List<Loadout> loadouts { get; set; }
+        public Item items { get; set; }
     }
 
     public class Equipment
-    {
-        public Data data { get; set; }
-        public int privacy { get; set; }
-    }
-
-    public class Inventory
     {
         public Data data { get; set; }
         public int privacy { get; set; }
@@ -32,37 +25,24 @@
         public int state { get; set; }
         public int dismantlePermission { get; set; }
         public bool isWrapper { get; set; }
-        public List<object> tooltipNotificationIndexes { get; set; }
+        public List<int> tooltipNotificationIndexes { get; set; }
         public int versionNumber { get; set; }
         public long? overrideStyleItemHash { get; set; }
-        public List<object> plugItemHashes { get; set; }
     }
 
-    public class Loadout
-    {
-        public object colorHash { get; set; }
-        public int iconHash { get; set; }
-        public object nameHash { get; set; }
-        public List<Item> items { get; set; }
-        public Data data { get; set; }
-        public int privacy { get; set; }
-    }
-
-    public class PHItemDataModelMessageData
+    public class BungieItemDataModelMessageData
     {
     }
 
-    public class PHItemDataModelResponse
+    public class BungieItemDataModelResponse
     {
-        public Inventory inventory { get; set; }
         public Equipment equipment { get; set; }
-        public Loadout loadouts { get; set; }
         public UninstancedItemComponents uninstancedItemComponents { get; set; }
     }
 
-    public class PHItemDataModel
+    public class BungieItemDataModel
     {
-        public Response Response { get; set; }
+        public BungieItemDataModelResponse Response { get; set; }
         public int ErrorCode { get; set; }
         public int ThrottleSeconds { get; set; }
         public string ErrorStatus { get; set; }
@@ -75,6 +55,5 @@
     }
 
 
-
-
 }
+
